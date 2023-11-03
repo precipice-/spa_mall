02eaf85fc45e94cd7bf583d0776c6dd2173a1da6
precipice- <configure62@gmail.com>
2023-11-03 22:00:02 +0900

    mongodb uri update

--- schemas/goods.js
+++ schemas/goods.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const goodsSchema = new mongoose.Schema({
-    goodsId: {
-        type: Number,
-        required: true,
-        unique: true,
-    },
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    thumbnailUrl: {
-        type: String,
-    },
-    category: {
-        type: String,
-    },
-    price: {
-        type: Number,
-    },
+  goodsId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  thumbnailUrl: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  price: {
+    type: Number
+  }
 });
 
-module.exports = mongoose.model('Goods', goodsSchema);
+module.exports = mongoose.model("Goods", goodsSchema);
